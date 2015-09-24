415b161a9f053eb890c9b3888b3f9cf6edc0a7ae
KarmaPanda <karmalifeofpanda@gmail.com>
2015-09-24 16:12:22 -0400

    Disable Watermark and EloBuddy Chat with this.

---
+++ DisableWaterMark/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DisableWaterMark
+{
+    using EloBuddy;
+
+    internal class Program
+    {
+        static void Main()
+        {
+            if (EloBuddy.Hacks.RenderWatermark)
+            {
+                EloBuddy.Hacks.RenderWatermark = false;
+            }
+            
+            if (EloBuddy.Hacks.IngameChat)
+            {
+                EloBuddy.Hacks.IngameChat = false;
+            }
+        }
+    }
+}
